f3e3205c0d10b83dda90382aa78c8913ee768447
jegerima <jegerima@gmail.com>
2022-10-10 11:58:06 -0500

    updated cypress config. node v14.17

--- cypress.config.js
+++ cypress.config.js
@@ -2,4 +2,9 @@ const {defineConfig} = require('cypress');
 
 module.exports = defineConfig({
   video: false,
+  e2e: {
+    baseUrl: 'http://localhost:8011',
+    supportFile: false,
+    specPattern: 'cypress/integration/app.js',
+  },
 });
